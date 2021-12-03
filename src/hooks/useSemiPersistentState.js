f1a088ffab27f78498b81dae2816f0b0da4534f2
mattborghi <borghi.matias@gmail.com>
2021-12-03 17:48:26 -0300

    minor optimization on useSemiPersistentState hook

--- src/hooks/useSemiPersistentState.js
+++ src/hooks/useSemiPersistentState.js
@@ -1,10 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // general hook independently of search
 export const useSemiPersistentState = (key, initialState) => {
+  const isMounted = useRef(false);
+
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
   useEffect(() => {
-    localStorage.setItem(key, value);
+    // Avoid calling useEffect on first render
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      localStorage.setItem(key, value);
+    }
   }, [value, key])
   return [value, setValue];
 }
